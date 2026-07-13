0f1349875e62a28bb03210b596a8417ad2521fcd
bugcolony <54467782+bugcolony@users.noreply.github.com>
2026-07-13 19:04:50 +0300

    codecrafters submit [skip ci]

--- commands/history.go
+++ commands/history.go
@@ -3,10 +3,11 @@ package commands
 import (
 	"fmt"
 	"io"
+	"strconv"
 )
 
 type HistoryLister interface {
-	List() [][]byte
+	List() []string
 }
 
 type History struct {
@@ -18,8 +19,19 @@ func (h *History) Name() string {
 }
 
 func (h *History) Execute(args []string, out io.Writer, errOut io.Writer) bool {
-	for i, line := range h.source.List() {
-		fmt.Fprintf(out, "%5d %s\n", i+1, string(line))
+	var offset int
+	list := h.source.List()
+
+	if len(args) == 1 {
+		n, err := strconv.Atoi(args[0])
+
+		if err == nil && n > 0 {
+			offset = len(list) - n
+		}
+	}
+
+	for i := offset; i < len(list); i++ {
+		fmt.Fprintf(out, "%5d %s\n", i+1, list[i])
 	}
 
 	return true
--- shell/history.go
+++ shell/history.go
@@ -7,7 +7,7 @@ import (
 
 type History struct {
 	file *os.File
-	mem  [][]byte
+	mem  []string
 }
 
 func NewHistory(filename string) (*History, func(), error) {
@@ -34,16 +34,16 @@ func (h *History) initMem() {
 	s := bufio.NewScanner(h.file)
 
 	for s.Scan() {
-		h.mem = append(h.mem, s.Bytes())
+		h.mem = append(h.mem, s.Text())
 	}
 }
 
 func (h *History) Push(line string) {
-	h.mem = append(h.mem, []byte(line))
+	h.mem = append(h.mem, line)
 
 	h.file.WriteString(line + "\n")
 }
 
-func (h *History) List() [][]byte {
+func (h *History) List() []string {
 	return h.mem
 }
